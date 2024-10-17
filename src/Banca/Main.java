8cab6fc7aeec960677f43ca63655de8097f2176a
CatBonzo <mahimapichini@libero.it>
2024-10-17 14:48:46 +0200

    ges
    -

--- src/Banca/Main.java
+++ src/Banca/Main.java
@@ -1,48 +1,92 @@
 package Banca;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-	
-	//menu
-		public static void menu1() {
-			System.out.println("------Banca-----------");
-			System.out.println("1) Utente non esistente");
-			System.out.println("2) Utente gia esistente-Inserire_saldo");
-			System.out.println("3) Ritirare saldo");			
-			System.out.println("--------------------");
-		}
-		
-	public static void main(String[] args) {
-		
-		menu1();
-		int valoreLetto = 0;
-		Scanner input = new Scanner(System.in);
-		valoreLetto = input.nextInt();
-		
-		switch(valoreLetto) {
-		
-			case 1: // Utente non esistente
-					System.out.println("Utente non esistente");
-					
-				//Creazione utente - iban ,intetsatario
-				//Riportare menu
-				break;
-			case 2: //Utente gia esistente
-					System.out.println("Utente gia esistente");
-					//Visualizzazioe saldo
-					//Inserimento saldo
-				break;
-			case 3: // Ritirare saldo
-					System.out.println("Ritirare saldo");
-					//Visualizzazioe saldo
-					//Operazione ritiro saldo
-				break;
-			case 4: 
-				break;
-		}
-		  
-	}
+    
+    // Menu
+    public static void menu() {
+        System.out.println("------Banca-----------");
+        System.out.println("1) Creazione conto");
+        System.out.println("2) Inserimento saldo");
+        System.out.println("3) Ritirare saldo");
+        System.out.println("4) Visualizzazione saldo");
+        System.out.println("5) Esci");
+        System.out.println("----------------------");
+    }
+    
+    public static void main(String[] args) {
+        
+        Banca b1 = new Banca();
+        Scanner input = new Scanner(System.in);
+        String intestatario = "";
+        int scelta = 0;
+        
+        // Ciclo infinito per mostrare il menu finché non si sceglie di uscire
+        while (scelta != 5) {
+            menu();
+            System.out.print("Scegli un'opzione: ");
+            scelta = input.nextInt();
+            input.nextLine(); // Consuma la linea rimasta vuota
+            
+            switch(scelta) {
+                case 1: // Creazione conto
+                    System.out.print("Intestatario: ");
+                    intestatario = input.nextLine();
+                    
+                    b1.AggiungiUtente(intestatario);
+                    b1.stampaConti();
+                    break;
+                    
+                case 2: // Inserimento saldo
+                    System.out.print("IBAN: ");
+                    String ibanDeposito = input.nextLine();
+                    
+                    System.out.print("Deposita: ");
+                    double deposito = input.nextDouble();
+                    
+                    Conto contoDeposito = b1.getConto(ibanDeposito);
+                    if (contoDeposito != null) {
+                        contoDeposito.deposita(deposito);
+           
+                    } else {
+                        System.out.println("Nessun conto trovato con questo IBAN.");
+                    }
+                    
+                    break;
+                    
+                case 3: // Ritirare saldo
+                    System.out.print("IBAN: ");
+                    String ibanPrelievo = input.nextLine();
+                    
+                    System.out.print("Importo da ritirare: ");
+                    double prelievo = input.nextDouble();
+                    
+                    Conto contoPrelievo = b1.getConto(ibanPrelievo);
+                    if (contoPrelievo != null) {
+                        contoPrelievo.prelievo(prelievo);
+               
+                    } else {
+                        System.out.println("Nessun conto trovato con questo IBAN.");
+                    }
+                    break;
+                    
+                case 4: // Visualizzazione saldo
+                    System.out.print("IBAN: ");
+                    String ibanVisualizza = input.nextLine();
+                    
+                    b1.visualizzazioneSaldo(ibanVisualizza);
+                    break;
+                
+                case 5: // Esci
+                    System.out.println("Uscita dal programma.");
+                    break;
+                
+                default:
+                    System.out.println("Scelta non valida. Riprova.");
+            }
+        }
+        
+        input.close();
+    }
 }
